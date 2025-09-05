994b5f84c6199e86ea17a294b56774ed23500802
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-09-05 23:28:53 +0500

    Added: login

--- v1/views.py
+++ v1/views.py
@@ -13,7 +13,7 @@ def status():
 
 
 @method
-def get_access_token(username=None, password=None):
+def login(username=None, password=None):
     # Dummy implementation, replace with real authentication logic
     if username == "admin" and password == "password":
         return {"access_token": "dummy-token-123"}
